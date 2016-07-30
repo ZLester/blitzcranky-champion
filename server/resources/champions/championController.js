44144d3d06e1ca3655610249f2295e6f89aa8f89
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-30 15:56:20 -0700

    Modify championController update method

--- server/resources/champions/championController.js
+++ server/resources/champions/championController.js
@@ -35,16 +35,15 @@ exports.updateOne = (req, res) => {
 exports.update = (req, res) => {
   Champion.remove({})
     .then(() => League.retrieveFreeChampions())
-    .then(freeChampions => {
-      return freeChampions
-        .map(freeChampion => League.retrieveChampionById(freeChampion.id));
-    })
+    .then(freeChampions => freeChampions
+        .map(freeChampion => League.retrieveChampionById(freeChampion.id)))
     .then(championRequests => Promise.all(championRequests))
     .then(champions => champions.map(addImageUrls))
     .then(champions => {
       const colorRequests = champions.map(champion => getChampionColors(champion));
       return Promise.all(colorRequests)
-        .then(colorsList => colorsList.map((colors, index) => Object.assign({}, champions[index], { colors })));
+        .then(colorsList => colorsList
+          .map((colors, index) => Object.assign({}, champions[index], { colors })));
     })
     .then(champions => Champion.create(champions))
     .then(champions => res.json(champions))
