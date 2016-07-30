3a44f7c7c87e6ddf03199610be606a9e78032ffc
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-30 16:16:42 -0700

    Update config file to include addition env vars

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3001,
   DB_URI: process.env.MONGODB_URI || 'mongodb://localhost/blitzcranky',
+  // Add your API key here when running locally
+  LEAGUE_API_KEY: process.env.LEAGUE_API_KEY || null,
 };
